d82305d62b33a96ddcdddd3411b3c9e8e79e7b6e
kiran2701 <gaurdian.kiran@gmail.com>
2024-01-24 16:45:36 +0530

    Added lights off ghost event

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int keysRequired;
+    [SerializeField] private SoundType soundType;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerView>() && keysRequired == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
 }
