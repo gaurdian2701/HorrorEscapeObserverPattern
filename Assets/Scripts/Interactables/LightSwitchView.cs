61072b659c724c75d6a1ab7867c4e0d159577b6f
kiran2701 <gaurdian.kiran@gmail.com>
2024-01-25 10:57:30 +0530

    Added UI listener to LightsSwitchedOffByGhostEvent

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -8,9 +8,17 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
-    private void OnEnable() => EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+    private void OnEnable()
+    {
+        EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+        EventService.Instance.OnLightsOffByGhostEvent.AddListener(onLightsOffByGhost);
+    }
 
-    private void OnDisable() => EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+    private void OnDisable()
+    {
+        EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+        EventService.Instance.OnLightsOffByGhostEvent.RemoveListener(onLightsOffByGhost);
+    }
 
     private void Start() => currentState = SwitchState.Off;
 
@@ -39,10 +47,28 @@ public class LightSwitchView : MonoBehaviour, IInteractable
         }
     }
 
+    private void SetLights(bool state)
+    {
+        foreach (Light lightSource in lightsources)
+        {
+            lightSource.enabled = state;
+        }
+        if (state)
+            currentState = SwitchState.On;
+        else
+            currentState = SwitchState.Off;
+    }
     private void onLightSwitch()
     {
         toggleLights();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
         GameService.Instance.GetInstructionView().HideInstruction();
+    }
+
+    private void onLightsOffByGhost()
+    {
+        SetLights(false);
+        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
+        GameService.Instance.GetInstructionView().ShowInstruction(InstructionType.LightsOff);
     }
 }
